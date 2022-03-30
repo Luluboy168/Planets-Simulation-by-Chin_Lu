2ea7a49c8f4f312defc9e0b9e63dfaf16dbdb479
hou ren hao <72810883+hourenhao1@users.noreply.github.com>
2022-03-30 09:34:32 +0800

    Update Planets-Simulation.py

--- Planets-Simulation.py
+++ Planets-Simulation.py
@@ -7,7 +7,7 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Planet Simulation by 晉祿")
+pygame.display.set_caption("Planet Simulation")
 #Color
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
